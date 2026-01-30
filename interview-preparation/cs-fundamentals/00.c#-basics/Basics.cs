b604289b0838b36d34d6ef70f1a6bbd73384c573
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-01-31 01:42:33 +0600

    completed Array, List, Dictionary

--- interview-preparation/cs-fundamentals/00.c#-basics/Basics.cs
+++ interview-preparation/cs-fundamentals/00.c#-basics/Basics.cs
@@ -50,12 +50,108 @@ namespace CsFundamentals
 
             int result = Add(a, b);
             Console.WriteLine("Sum of a and b: " + result);
+
+
+            int[] numbers = new int[] {5, 2, 4, 3, 1};
+            
+            Array.Sort(numbers);
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine("Number: " + num);
+            }
+
+            Array.Reverse(numbers);
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine("Number: " + num);
+            }
+
+            string s = "Hello C#";
+            Console.WriteLine("String s: " + s);
+
+            // palindrome check
+            string rev = "";
+            char[] cArray = s.ToCharArray();
+            Array.Reverse(cArray);
+            rev = new string(cArray);
+            Console.WriteLine("Reversed string: " + rev);
+
+            if(s == rev)
+            {
+                Console.WriteLine("The string is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not a palindrome.");
+            }
+
+            IList<int> list = new List<int>();
+
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            Console.WriteLine("Index 1 item: " + list[1]);
+            foreach(int item in list)
+            {
+                Console.WriteLine("List item: " + item);
+            }
+
+            IList<string> strList = new List<string>() {"apple", "banana", "cherry"};
+
+            foreach(string fruit in strList)
+            {
+                Console.WriteLine("Fruit: " + fruit);
+            }
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict["one"] = 1;
+            dict["two"] = 2;
+            dict["three"] = 3;
+
+            foreach(var kvp in dict)
+            {
+                Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
+                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+            }
+
+            foreach(KeyValuePair<string, int> kvp in dict)
+            {
+                Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
+            }
+
+            string[] key = new string[] {"red", "green", "blue"};
+            int[] value = new int[] {1, 2, 3};
+
+            for(int i = 0; i < key.Length; i++)
+            {
+                dict.Add(key[i], value[i]);
+                // way 2
+                // dict[key[i]] = value[i];
+                // way 3
+                // dict.TryAdd(key[i], value[i]);
+                // way 4
+                // dict.Append(new KeyValuePair<string, int>(key[i], value[i]));
+            }
+
+            foreach(var kvp in dict)
+            {
+                Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
+            }
+
+            dict.Remove("two");
+            Console.WriteLine("After removing key 'two':");
+            foreach(var kvp in dict)
+            {
+                Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
+            }
         }
 
         static int Add(int x, int y)
         {
             return x + y;
         }
-
     }
 }
